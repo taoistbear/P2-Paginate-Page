42563deda3c91259f61eff5f37ed8e5f5d7e22cc
TaoistBear <originaltaoistbear@gmail.com>
2016-09-18 14:27:59 -0400

    setting up buttons
    set up to begin pagination

--- js/student_search.js
+++ js/student_search.js
@@ -11,4 +11,31 @@
     var pageNum = Math.floor((fArr.length / show) + 1);
     return pageNum;
   }
+  // when page loads set up first function to divide the array and create pages
+  function paginate(nArr) {
+    //sets up how many page buttons there should be and creates the html
+    var buttons = pages(nArr, 10);
+    var $pageDiv = '<div class="pagination"><ul>';
+    for ( var i = 0; i < buttons; i++ ) {
+        $pageDiv += '<li><a href="#">' + (i+1) + '</a></li>';
+    }
+    $pageDiv += '</li></div>';
+    $('.page').append($pageDiv);
+
+    //set click handlers for each button and force click first one.
+    $('div.pagination ul li').each(function(nArr) {
+      $('a').on('click', function(nArr) {
+        $('a').removeClass('active');
+        $(this).addClass('active');
+        $('ul.student-list li.student-item').each(function() {
+          $(this).addClass('hide');
+        });
+        var idx = (($(this).index())*10);
+        
+      });
+    });
+  }
+
+  paginate(studArr);
+
 }());
